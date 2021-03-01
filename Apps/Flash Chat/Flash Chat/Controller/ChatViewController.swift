e2074c8c4ff0d5dfa8fd5931c096a50e7e42244d
Lemonbrush <aa_rubtsov@mail.ru>
2021-03-01 15:37:51 +0300

    Flash Chat can send messages to the Firebase cloud

--- Apps/Flash Chat/Flash Chat/Controller/ChatViewController.swift
+++ Apps/Flash Chat/Flash Chat/Controller/ChatViewController.swift
@@ -13,15 +13,42 @@ class ChatViewController: UIViewController {
     @IBOutlet weak var tableView: UITableView!
     @IBOutlet weak var messageTextfield: UITextField!
     
+    let db = Firestore.firestore()
+    
+    var messages: [Message] = [
+        Message(sender: "1@2.mail.com", body: "Hey!"),
+        Message(sender: "a@b.mail.com", body: "Hello"),
+        Message(sender: "1@2.mail.com", body: "Whay's up? Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.")
+    ]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        tableView.dataSource = self
 
         // Top bar menu setup 
         navigationItem.hidesBackButton = true
         navigationItem.title = "⚡️FlashChat"
+        
+        tableView.register(UINib(nibName: K.cellNibName, bundle: nil), forCellReuseIdentifier: K.cellIdentifier)
     }
 
     @IBAction func sendPressed(_ sender: Any) {
+        
+        if let messageBody = messageTextfield.text,
+           let messageSender = Auth.auth().currentUser?.email {
+            
+            db.collection(K.FStore.collectionName).addDocument(data:
+                                                                [K.FStore.senderField: messageSender,
+                                                                 K.FStore.bodyField: messageBody]) { (error) in
+                if let e = error {
+                    print("There was an issue saving data to firestore, \(e)")
+                } else {
+                    print("Successfully saved data.")
+                }
+            }
+            
+        }
         
     }
     
@@ -36,4 +63,21 @@ class ChatViewController: UIViewController {
         
     }
     
+}
+
+//MARK: - Tableview
+extension ChatViewController: UITableViewDataSource {
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return messages.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: K.cellIdentifier, for: indexPath) as! MessageCell
+        
+        cell.label.text = messages[indexPath.row].body
+        
+        return cell
+    }
+
 }
---
+++ Apps/Flash Chat/Flash Chat/Model/Message.swift
@@ -0,0 +1,13 @@
+//
+//  Message.swift
+//  Flash Chat
+//
+//  Created by Александр on 01.03.2021.
+//
+
+import Foundation
+
+struct Message {
+    let sender: String
+    let body: String
+}
