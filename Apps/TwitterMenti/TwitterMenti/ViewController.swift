3fecd46e56806ad6afb461c104c0eb0c2be04645
Lemonbrush <aa_rubtsov@mail.ru>
2021-03-27 12:09:54 +0300

    Add starter TwitterMenti project

---
+++ Apps/TwitterMenti/TwitterMenti/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  TwitterMenti
+//
+//  Created by Александр on 27.03.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    @IBOutlet weak var textField: UITextField!
+    @IBOutlet weak var sentimentLabel: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+
+    }
+
+    @IBAction func predictPressed(_ sender: UIButton) {
+        
+    }
+    
+}
+
