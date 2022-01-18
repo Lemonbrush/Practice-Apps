1c1a4e35672bcbc542b2a70676f1108065507ca3
Lemonbrush <aa_rubtsov@mail.ru>
2022-01-18 19:30:46 +0300

    add serialization project pt2

--- Playground/serialization.playground/Pages/Nested unkeyed containers.xcplaygroundpage/Contents.swift
+++ Playground/serialization.playground/Pages/Nested unkeyed containers.xcplaygroundpage/Contents.swift
@@ -38,8 +38,33 @@ struct Toy: Encodable {
   enum CodingKeys: CodingKey {
     case name, label
   }
+    
+    func encode(to encoder: Encoder) throws {
+        var container = encoder.container(keyedBy: CodingKeys.self)
+        try container.encode(name, forKey: .name)
+        var labelContainer = container.nestedUnkeyedContainer(forKey: .label)
+        try labelContainer.encode(name.lowercased())
+        try labelContainer.encode(name.uppercased())
+        try labelContainer.encode(name)
+    }
+}
+
+extension Toy: Decodable {
+    init(from decoder: Decoder) throws {
+        let container = try decoder.container(keyedBy: CodingKeys.self)
+        name = try container.decode(String.self, forKey: .name)
+        
+        var labelContainer = try container.nestedUnkeyedContainer(forKey: .label)
+        var labelName = ""
+        while !labelContainer.isAtEnd {
+            labelName = try labelContainer.decode(String.self)
+        }
+        label = labelName
+    }
 }
 
 let toy = Toy(name: "Teddy Bear", label: "Teddy Bear")
 let data = try encoder.encode(toy)
 let string = String(data: data, encoding: .utf8)!
+
+let sameToy = try decoder.decode(Toy.self, from: data)
--- Playground/serialization.playground/Pages/Unkeyed containers.xcplaygroundpage/Contents.swift
+++ Playground/serialization.playground/Pages/Unkeyed containers.xcplaygroundpage/Contents.swift
@@ -41,10 +41,25 @@ struct Label: Encodable {
   var toy: Toy
     
     func encode(to encoder: Encoder) throws {
-        <#code#>
+        var container = encoder.unkeyedContainer()
+        try container.encode(toy.name.lowercased())
+        try container.encode(toy.name.uppercased())
+        try container.encode(toy.name)
+    }
+}
+
+extension Label: Decodable {
+    init(from decoder: Decoder) throws {
+        var container = try decoder.unkeyedContainer()
+        var name = ""
+        while !container.isAtEnd {
+            name = try container.decode(String.self)
+        }
+        toy = Toy(name: name)
     }
 }
 
 let label = Label(toy: toy)
 let labelData = try encoder.encode(label)
 let labelString = String(data: labelData, encoding: .utf8)!
+let sameLabel = try decoder.decode(Label.self, from: labelData)
